4856bc72ea0d30eccf23590f89b20d195f64c5cd
Shravya Mogaveera <shravyamogaveera3@gmail.com>
2026-03-28 15:43:43 +0530

    Added functions to add and get tasks

--- app.py
+++ app.py
@@ -23,3 +23,28 @@ def init_db():
                     created_at      TEXT    DEFAULT CURRENT_TIMESTAMP
                 )
         """)
+    
+# CRUD FUNCTIONS
+def add_task(title,description,priority,status,due_date):
+    conn= get_connection()
+    conn.execute(" INSERT INTO tasks(title,description,priority,status,due_date) VALUES (?,?,?,?,?)",
+                 (title,description,priority,status,due_date))
+    conn.commit(); conn.close()
+
+def get_tasks(status_filter="ALL",priority_filter="ALL",search=""):
+    conn=get_connection()
+    query="SELECT * FROM tasks WHERE 1=1"
+    params=[]
+    if status_filter !="ALL":
+        query +=" AND status = ?"
+        params.append(status_filter)
+    if priority_filter !="ALL":
+        query +=" AND priority = ?"
+        params.append(priority_filter)
+    if search:
+        query += "AND (title LIKE ? OR description LIKE ?)"
+        params += [f"%{search}%", f"%{search}"]
+    query += " ORDER BY created_at DESC"
+    rows= conn.execute(query,params).fetchall()
+    conn.close()
+    return rows
